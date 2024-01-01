e2e46e61e1634459d0b9cc9f410294969011daa1
r14dd <riadmukh@buffalo.edu>
2024-01-01 18:27:03 -0500

    resolved 'portfolio issue'

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-   
-  </React.StrictMode>
+   ,
+  </React.StrictMode>,
+  document.getElementById(root)
 );
 
 // If you want to start measuring performance in your app, pass a function
